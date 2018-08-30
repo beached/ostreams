2cfd0061c20d0143db2df96a5ff579a99bdb56f0
Darrell Wright <dwright-git@dawdevel.ca>
2018-08-30 17:44:03 -0400

    .

--- tests/memory_test.cpp
+++ tests/memory_test.cpp
@@ -25,59 +25,60 @@
 #include "console_stream.h"
 #include "memory_stream.h"
 
-template<size_t buff_sz, typename CharT = char>
-class buffer_t {
-	CharT value[buff_sz + 1] = {0};
-	static constexpr size_t sz = buff_sz;
+namespace {
+	template<size_t buff_sz, typename CharT = char>
+	class buffer_t {
+		CharT value[buff_sz + 1] = {0};
+		static constexpr size_t sz = buff_sz;
 
-public:
-	constexpr size_t size( ) const noexcept {
-		return sz;
+	public:
+		constexpr size_t size( ) const noexcept {
+			return sz;
+		}
+		constexpr CharT const *data( ) const noexcept {
+			return value;
+		}
+		constexpr CharT *data( ) noexcept {
+			return value;
+		}
+
+		constexpr buffer_t( ) noexcept = default;
+
+		constexpr operator CharT const *( ) const noexcept {
+			return data( );
+		}
+	};
+
+	struct A {};
+
+	// Only need to define a structure that has data( ) and size( ) members
+	// This includes std::basic_string, std::basic_string_view,
+	// daw::basic_string_view
+	template<typename CharT>
+	constexpr auto to_string( A ) noexcept {
+		buffer_t<1> result;
+		result.data( )[0] = 'A';
+		return result;
 	}
-	constexpr CharT const *data( ) const noexcept {
-		return value;
+
+	template<size_t buff_sz = 74, typename Float>
+	constexpr auto test( Float f ) {
+		// If buffer isn't large enough a buffer_full_exception will be thrown
+		buffer_t<76> buffer{};
+		auto buff_os =
+		  ::daw::io::make_memory_buffer_stream( buffer.data( ), buffer.size( ) );
+		buff_os << A{} << " The number is: " << f << ". " << 5
+		        << " times number is " << ( static_cast<Float>( 5 ) * f ) << '\n';
+
+		return buffer;
 	}
-	constexpr CharT *data( ) noexcept {
-		return value;
-	}
-
-	constexpr buffer_t( ) noexcept = default;
-
-	constexpr operator CharT const *( ) const noexcept {
-		return data( );
-	}
-};
-
-struct A {};
-
-// Only need to define a structure that has data( ) and size( ) members
-// This includes std::basic_string, std::basic_string_view,
-// daw::basic_string_view
-template<typename CharT>
-constexpr auto to_string( A ) noexcept {
-	buffer_t<1> result;
-	result.data( )[0] = 'A';
-	return result;
-}
-
-template<size_t buff_sz = 74, typename Float>
-constexpr auto test( Float f ) {
-	// If buffer isn't large enough a buffer_full_exception will be thrown
-	buffer_t<76> buffer{};
-	auto buff_os =
-	  ::daw::io::make_memory_buffer_stream( buffer.data( ), buffer.size( ) );
-	buff_os << A{} << " The number is: " << f << ". " << 5 << " times number is "
-	        << ( static_cast<Float>( 5 ) * f ) << '\n';
-
-	return buffer;
-}
+} // namespace
 
 constexpr static auto const test_result = test( 1234560.435333 );
 
 auto const &test2( ) {
 	return test_result;
 }
-
 int main( int argc, char ** ) {
 	daw::con_out << test2( ) << '\n';
 	return 0;
